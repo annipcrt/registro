e9b23a8c08f6527fdc942200cc00bb23183b8377
German Maximo Montero Sande <german.montero@fing.edu.uy>
2023-08-09 13:36:56 -0300

    tos

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,16 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function tos() {
+    return document.getElementById("terminos").checked;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("regBtn").addEventListener("click",e => {
+        if(tos())
+            showAlertSuccess();
+        else
+            showAlertError();
+    });
+});
