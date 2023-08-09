aee30adcd7c5866ec0fea92568432c6a0b98ed3b
Santiago <santiagocortondo25@gmail.com>
2023-08-09 13:31:58 -0300

    Contrasenias
    Los datos ingresados en "Contraseña" y "Repetir contraseña" deben ser iguales.

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,13 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById('regBtn').addEventListener('click', function() {
+    var password1 = document.getElementById('password1').value;
+    var password2 = document.getElementById('password2').value;
+    if (password1 === password2 ) {
+        showAlertSuccess();
+    } else {
+        showAlertError();
+    }
+});
