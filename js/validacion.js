defcbc65f57f624b8442d96f3a3210a2d35fe925
German Maximo Montero Sande <german.montero@fing.edu.uy>
2023-08-09 15:53:42 -0300

    separe verificar Iguales en su propia func

--- js/validacion.js
+++ js/validacion.js
@@ -5,6 +5,7 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
 function tos() {
     return document.getElementById("terminos").checked;
 }
@@ -14,19 +15,21 @@ function verificarLongitud() {
     return clave.length >= 6;
 }
 
-
-function vertificarCampos(){
+function vertificarCampos() {
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
     let email = document.getElementById("email").value;
     return !(nombre === "" || apellido === "" || email === "") 
-         }
-document.getElementById('regBtn').addEventListener('click', function() {
+}
 
-        
+function verificarIguales() {
     var password1 = document.getElementById('password1').value;
     var password2 = document.getElementById('password2').value;
-    if (password1 === password2 && tos() && verificarLongitud() && vertificarCampos()) {
+    return password1 === password2;
+}
+
+document.getElementById('regBtn').addEventListener('click', function() {    
+    if (verificarIguales() && tos() && verificarLongitud() && vertificarCampos()) {
         showAlertSuccess();
     } else {
         showAlertError();
